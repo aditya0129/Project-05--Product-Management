d584ce45971f013b57958ee60a57bf2ba12503d1
AbhishekChauhan9036 <abhishekchauhan9036@gmail.com>
2023-02-01 19:54:34 +0530

    Done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -80,6 +80,61 @@ const register=async function(req,res){
 }
 
 
+
+
+
+
+const userLogin = async function (req, res) {
+    try {
+        let { email, password } = req.body;
+
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).send({ status: false, message: "please input user Details" });
+        }
+
+        if (!email) {
+            return res.status(400).send({ status: false, message: "EmailId is mandatory", });
+        }
+
+        if (!validEmail(email)) {
+            return res.status(400).send({ status: false, message: "EmailId should be Valid", });
+        }
+
+        if (!password) {
+            return res.status(400).send({ status: false, message: "Password is mandatory" });
+        }
+
+        if (password.length <=8 || password.length >=15) {
+            return res.status(400).send({ status: false, message: "the length of password must be min: 8 and max: 15", });
+        }
+
+        let verifyUser = await userModel.findOne({ email: email });
+        if (!verifyUser){
+            return res.status(400).send({ status: false, message: "Invalid Login Credential" });
+        }
+
+        bcrypt.compare(password, verifyUser.password, function (error, verifyUser) {
+            if (error) {
+                return res.status(400).send({ status: false, message: error.message })
+            }else {
+            verifyUser == true
+          }
+        });
+
+
+        let payload = { userId: verifyUser["_id"], iat: Date.now(), };
+        let token = jwt.sign(payload, "group-13-project", { expiresIn: "2h" });
+
+        res.setHeader("authorization", token);
+        res.status(200).send({ status: true, message: "User login successfull", data: { userId: verifyUser["_id"], token } });
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message, });
+    }
+};
+
+
+
+
 const getUsers = async function (req, res) {
     try {
         let userId = req.params.userId;
@@ -98,4 +153,4 @@ const getUsers = async function (req, res) {
   }
 };
 
-        module.exports={register,getUsers}
+        module.exports={register,getUsers,userLogin}
