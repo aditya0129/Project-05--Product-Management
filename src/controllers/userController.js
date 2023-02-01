32339333ce587232f6ecb0d8fd2c12606c17d27a
aditya0129 <adityakumar00129@gmail.com>
2023-02-01 19:51:31 +0530

    post-api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,89 @@
-const getUsers = async function (req, res) {
-  try {
-    let userId = req.params.userId;
+const usermodel=require("../models/userModel")
+const { validateName, validateEmail, validatePassword, validateMobileNo, validatePincode, validatePlace }=require("../validation/validation")
+const validator=require("validator")
 
+
+const register=async function(req,res){
+   try{ 
+    let data=req.body
+    
+    if(Object.keys(data).length == 0) return res.status(400).send({status:false,message:"please provide fields"}) 
+
+    let {fname,lname,email,profileImage,phone,password,address}=data
+
+    if(!fname) return res.status(400).send({status:false,message:"Please provide fname"})
+
+    if(!validateName(fname).trim()) return res.status(400).send({status:false,message:"Please provide valid  fname"})
+    
+    if(!lname) return res.status(400).send({status:false,messsage:"please provide lname"})
+
+    if(!validateName(lname).trim()) return res.status(400).send({status:false,message:"Please provide valid  lname"})
+
+    if(!email) return res.status(400).send({status:false,messsage:"please provide email"})
+
+    if(!validateEmail(email).trim()) return res.status(400).send({status:false,messsage:"please provide valid email"})
+
+    if(!profileImage) return res.status(400).send({status:false,messsage:"please provide profileImage"})
+
+    if(!validator.isURL(profileImage).trim()) return res.status(400).send({status:false,messsage:"please provide valid profileImage URL"})
+
+    if(!phone) return res.status(400).send({status:false,messsage:"please provide phone"})
+
+    if(!validateMobileNo(phone).trim()) return res.status(400).send({status:false,messsage:"please provide valid phone Number"})
+
+    if(!password) return res.status(400).send({status:false,messsage:"please provide password"})
+
+    if(!validatePassword(password).trim()) return res.status(400).send({status:false,messsage:"please provide valid password"})
+
+
+    if(!address) return res.status(400).send({status:false,messsage:"please provide address"})
+
+    if(!address.shipping) return res.status(400).send({status:false,messsage:"please provide shipping"})
+
+    if(!address.shipping.street) return res.status(400).send({status:false,messsage:"please provide street"})
+
+    if(!validateName(address.shipping.street).trim()) return res.status(400).send({status:false,messsage:"please provide valid street"})
+
+    if(!address.shipping.city) return res.status(400).send({status:false,messsage:"please provide city"})
+
+    if(!validateName(address.shipping.city).trim()) return res.status(400).send({status:false,messsage:"please provide city"})
+
+    if(!address.shipping.pincode) return res.status(400).send({status:false,messsage:"please provide pincode"})
+
+    if(!validator.isNumeric(address.shipping.pincode).trim()||address.shipping.pincode.length!=6)
+        {
+           return res.status(400).send({status:false,message:"make sure pincode should be numeric only and 6 digit number"})
+        }
+
+    if(!address.billing) return res.status(400).send({status:false,messsage:"please provide billing"})
+
+    if(!address.billing.street) return res.status(400).send({status:false,messsage:"please provide street"})
+
+    if(!validateName(address.billing.street).trim()) return res.status(400).send({status:false,messsage:"please provide valid street"})
+
+    if(!address.billing.city) return res.status(400).send({status:false,messsage:"please provide city"})
+
+    if(!validateName(address.billing.city).trim()) return res.status(400).send({status:false,messsage:"please provide valid city"})
+
+    if(!address.billing.pincode) return res.status(400).send({status:false,messsage:"please provide pincode"})
+
+    if(!validator.isNumeric(!address.billing.pincode).trim()||address.billing.pincode.length!=6)
+        {
+           return res.status(400).send({status:false,message:"make sure pincode should be numeric only and 6 digit number"})
+        }
+
+    let sdata= await usermodel.create(data)
+    res.status(200).send({status:true,message:"User created successfully",data:sdata})
+}catch(error){
+    res.status(500).send({status:false,message:error.message})
+}
+}
+
+
+const getUsers = async function (req, res) {
+    try {
+        let userId = req.params.userId;
+        
     if (!mongoose.isValidObjectId(userId))
       return res.status(400).send({ status: false, message: "user is invalid" });
 
@@ -15,3 +97,5 @@ const getUsers = async function (req, res) {
     return res.status(500).send({ status: false, message: error.message});
   }
 };
+
+        module.exports={register,getUsers}
