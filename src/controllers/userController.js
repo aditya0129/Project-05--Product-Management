d431fbf0d2d406df69c7c77e1af9fd739e84df8c
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-02-01 17:45:19 +0530

    get api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,17 @@
-fdg
+const getUsers = async function (req, res) {
+  try {
+    let userId = req.params.userId;
+
+    if (!mongoose.isValidObjectId(userId))
+      return res.status(400).send({ status: false, message: "user is invalid" });
+
+    let getData = await usermodle.findOne({ _id: userId });
+    if (!getData)
+      return res.status(404).send({ status: false, message: "user not found" });
+    return res
+      .status(200)
+      .send({ status: true, message: "User profile details", data: getData });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message});
+  }
+};
