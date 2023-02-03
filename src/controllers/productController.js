57d537a57092d28632b2d77a2a5645b4b3646b92
Soumyaranjanp68 <soumyaranjanp68@gmail.com>
2023-02-03 17:31:36 +0530

    getById

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,17 +1,42 @@
 const productModel = require("../models/productModel");
 const mongoose = require("mongoose");
 
-let product = async function (req, res) {
+let createProduct = async function (req, res) {
   try {
 
-    let createProduct = await productModel.create(data);
+    let savedProduct = await productModel.create(data);
 
     return res
       .status(201)
-      .send({ status: true, message: "Success", data: createProduct });
+      .send({ status: true, message: "Success", data: savedProduct });
   } catch (error) {
     res.status(500).send({ status: false, message: error.message });
   }
 };
 
-module.exports = { product };
+
+
+
+const getProductById = async function (req, res){
+
+  try {
+
+      let productId = req.params.productId
+
+      if (!isValidObjectId(productId)) return res.status(400).send({ status: false, message: `Please Enter Valid ProductId: ${productId}.` })
+
+     
+
+      let getProduct = await productModel.findOne({ _id: productId, isDeleted: false })  
+
+      if (!getProduct) return res.status(404).send({ status: false, message: "Product Data is Not Found!" })
+
+      return res.status(200).send({ status: true, message: "Success", data: getProduct })
+
+  } catch (error) {
+
+      return res.status(500).send({ status: false, message: error.message })
+  }
+}
+
+module.exports = { createProduct , getProductById };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { register,  getUsers, userLogin, updateUser } = require("../controllers/userController");
+const {createProduct , getProductById } = require("../controllers/productController")
 const { isAuthenticated, isAuthorized } = require("../middleware/commonMiddleware")
 
 
@@ -7,7 +8,9 @@ const { isAuthenticated, isAuthorized } = require("../middleware/commonMiddlewar
 router.post("/register",register);
 router.post("/login",userLogin);
 router.get("/user/:userId/profile",getUsers);
-router.post("/user/:userId/profile" ,isAuthenticated, isAuthorized, updateUser)
+router.post("/user/:userId/profile" ,isAuthenticated, isAuthorized, updateUser);
+router.get("/products/:productId" , getProductById);
+
 
 
 
